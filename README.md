a3fa5b22da2f2f7868448a8e5909fc454782e004
Dawid Sobczak <dawidsobczak@fastmail.com>
2023-09-22 16:04:15 +0100

    Hello, World!

---
+++ README.md
@@ -0,0 +1,14 @@
+
+Run dev server
+
+```
+python manage.py runserver
+```
+
+Manual command line setup
+
+```bash
+python -m venv ./venv
+source ./venv/bin/activate
+python -m pip -r requirements.txt
+````
--- forum/urls.py
+++ forum/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mysite project.
+URL configuration for forum project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('', include("app.urls")),
     path('admin/', admin.site.urls),
 ]
