cdb2c464f66cd349f67b037af7859a42c5a3e1d9
TZajas <20278748@studentmail.ul.ie>
2023-10-31 11:44:29 +0000

    Added class to DBManager class

--- src/authentication.py
+++ src/authentication.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
-from src.db import User
-from src.db_query_manager import create_user
+from src.db_query_manager import DBManager
 
 auth = Blueprint('authentication', __name__)
+
+db_manager = DBManager()
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
@@ -17,16 +18,16 @@ def register():
         else:
             user_type_id = 1
             
-        existing_user = User.query.filter_by(username=username).first()
+        existing_user = db_manager.get_user_by_username(username)
         
         if existing_user:
             flash('Username is already in use. Please choose a different one.')
         else:
-            existing_email = User.query.filter_by(email=email).first()
+            existing_email = db_manager.get_user_by_email(email)
             if existing_email:
                 flash('Email is already registered. Please use a different email.')
             else:
-                create_user(username, email, password, user_type_id)
+                db_manager.create_user(username, email, password, user_type_id)
                 flash('Registration successful! You can now log in.')
                 return redirect(url_for('authentication.login'))
             
@@ -38,7 +39,7 @@ def login():
         username = request.form.get('username')
         password = request.form.get('password')
         
-        user = User.query.filter_by(username=username).first()
+        user = db_manager.get_user_by_username(username)
         if user:
             if user.password == password:
                 session['user_id'] = user.user_id
--- src/db_query_manager.py
+++ src/db_query_manager.py
@@ -3,13 +3,28 @@ from src.db import *
 from src.db import db
 import uuid
 
-# Function to add a user with the "student" user type
-def create_user(username, email, password, user_type):
-    student_id = str(uuid.uuid1())
+class DBManager:
+    # Function to add a user to db
+    def create_user(self, username, email, password, user_type):
+        student_id = str(uuid.uuid1())
 
-    # Create a new user with the "student" user type
-    student_user = User(user_id=student_id, username=username, email=email, password=password, is_admin=0, user_type_id=user_type, is_blocked=0)
+        # Create a new user
+        student_user = User(user_id=student_id, username=username, email=email, password=password, is_admin=0, user_type_id=user_type, is_blocked=0)
+        
+        db.session.add(student_user)
+        db.session.commit()
+        
+    def get_user_by_id(self, user_id):
+        # Query the database to get a user by their user_id
+        user = User.query.filter_by(user_id=user_id).first()
+        return user
+
+    def get_user_by_username(self, username):
+        # Query the database to get a user by their username
+        user = User.query.filter_by(username=username).first()
+        return user
     
-    db.session.add(student_user)
-    db.session.commit()
-    
+    def get_user_by_email(self, email):
+        # Query the database to get a user by their email
+        user = User.query.filter_by(email=email).first()
+        return user
