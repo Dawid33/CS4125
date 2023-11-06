d4c5e17f95460e6c1d800c934ac96ae9356ff3c6
Dawid Sobczak <dawidsobczak@fastmail.com>
2023-11-06 08:33:49 +0000

    adding book details

--- dev.py
+++ dev.py
@@ -3,6 +3,7 @@ from models.db import db
 from src.authentication import auth
 from src.user_profile import profile
 from src.search import search
+from src.book import book
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -14,6 +15,7 @@ db.init_app(app)
 app.register_blueprint(auth)
 app.register_blueprint(profile)
 app.register_blueprint(search)
+app.register_blueprint(book)
 
 @app.route("/")
 def health_check():
