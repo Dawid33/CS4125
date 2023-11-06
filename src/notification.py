567484e960331751cc5cc440ac22938ff732bc82
TZajas <20278748@studentmail.ul.ie>
2023-11-06 15:24:56 +0000

    Added Catalogue and LendingManager Objects

--- src/notification.py
+++ src/notification.py
@@ -4,6 +4,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from os import getenv
 import smtplib
+import datetime
 
 notify = Blueprint('notify', __name__)
 
