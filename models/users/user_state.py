11dfe6e59cc69e5c9a2955e4bbb95da6b085ee2a
Farran-H <farranhaley14@gmail.com>
2023-11-07 20:54:08 +0000

    Added base code for the block method in admin.py and db_manager

---
+++ models/users/user_state.py
@@ -0,0 +1,32 @@
+from abc import ABC, abstractmethod
+
+# Abstract State
+class UserState(ABC):
+    @abstractmethod
+    def request_access(self):
+        pass
+
+# Concrete State: Blocked
+class BlockedState(UserState):
+    def blocked(self):
+        print("Access Denied: Your account has been blocked\n")
+
+# Concrete State: Unblocked
+class UnblockedState(UserState):
+    def unblocked(self):
+        print("Access Granted. Welcome!\n")
+
+# Context Class
+class User:
+    def __init__(self):
+        # User starts in an Unblocked State
+        self.state = UnblockedState()
+
+    def request_access(self):
+        self.state.request_access
+
+    def block_user(self):
+        self.state = BlockedState()
+
+    def unblock_user(self):
+        self.state = UnblockedState()
