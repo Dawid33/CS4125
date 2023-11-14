a530d1fb5efa12baa75039400a18fcb68e657522
Farran-H <farranhaley14@gmail.com>
2023-11-14 19:22:17 +0000

    User State Interface and Concrete Class

--- models/users/admin.py
+++ models/users/admin.py
@@ -1,25 +1,20 @@
 from models.users.user import User
-from models.catalogue.catalogue_manager import Catalogue
 from models.database_manager.db_manager import DBManager
 
 # Admin user type controls the book catalogue, and can also block/unblock users and waive user fines
 # Extends the User class
 class Admin(User):
-    def __init__(self, user_id, username, email, password):
-        super().__init__(user_id, username, email, password)
-        
-        self.catalogue_manager = Catalogue()
-        self.db_manager = DBManager()
-        self.user_type = 'ADMIN'
-    
-    def get_user_type(self):
-        return self.user_type
+    def __init__(self, Id, username, email, password, catalogue_manager):
+        super().__init__(Id, username, email, password)
+        # self.catalogue_manager = catalogue_manager
     
     def block_library_member(self, user):
-        self.db_manager.block_user(user)
+        block = DBManager.block_user(self, user)
+        return
+    
     
     def unblock_library_member(self, user):
-        self.db_manager.unblock_user(user)
+        pass
     
     def waive_fine(self, user, fine_id):
         pass
